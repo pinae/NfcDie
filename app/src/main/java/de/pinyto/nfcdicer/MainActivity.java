68d6c55ba5ea65ff84883dbdac8494c96989fe66
pinae <jonny@pinae.net>
2015-11-06 11:02:30 +0100

    Basic NFC detection

--- app/src/main/java/de/pinyto/nfcdicer/MainActivity.java
+++ app/src/main/java/de/pinyto/nfcdicer/MainActivity.java
@@ -1,13 +1,39 @@
 package de.pinyto.nfcdicer;
 
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tagIdView;
+    private NfcAdapter nfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tagIdView = (TextView) findViewById(R.id.textView_tag);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        if (!nfcAdapter.isEnabled()) {
+            tagIdView.setText(R.string.nfc_disabled);
+        }
+
+        handleIntent(getIntent());
+    }
+
+    private void handleIntent(Intent intent) {
+
     }
 }
